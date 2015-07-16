49a5d70b41cb486f70f52fac42aac8848624bca1
quangchito <quangchito@gmail.com>
2015-07-16 09:37:31 +0700

    update connect to server scala factor+rating

--- js/angular/OBJ/models.js
+++ js/angular/OBJ/models.js
@@ -1,14 +1,11 @@
 /**
  * Created by quang on 7/9/2015.
  */
-var modelinfo={
+var modelinfos={
+    _id:'',
     name:'',
     description:'',
     maxscore: 0,
     minscore:0,
     status:'DeActive'
-};
-var modelinfos={
-    _id:'',
-    model:modelinfo
 };
--- js/angular/OBJ/ratings.js
+++ js/angular/OBJ/ratings.js
@@ -1,19 +1,17 @@
 /**
  * Created by quang on 7/12/2015.
  */
-var rating={
-    modelid:'',
+
+var ratings={
     code:'',
     scorefrom : 0.0,
     scoreto : 0.0,
     status : '',
-    note:'',
-    createddate:null,
-    createdby:'',
-    modifieddate:null,
-    modifiedby:'',
+    statusname: '',
+    note:"",
 };
-var ratings={
-    _id:'',
-    rating:rating
+var modelforrating={
+    modelid:'',
+    modelname:'',
+    _id:''
 };
