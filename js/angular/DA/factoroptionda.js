cff542807dfd1ebb432a0c2a8b096a507c834a29
quangchito <quangchito@gmail.com>
2015-07-18 01:32:28 +0700

    update redirect factoroption

--- js/angular/DA/factoroptionda.js
+++ js/angular/DA/factoroptionda.js
@@ -7,6 +7,11 @@ function modellistangular($scope,$http,url)
 		.success(function (data) {
 			//alert(data);
 			$scope.modelinfos = data["ModelInfosList"];
+			//alert($scope.choiceFactor);
+			if($scope.choiceModel!=''&&$scope.choiceFactor!='') {
+				//alert('AAA');
+				backmodelChanged($scope, $http, $scope.choiceModel, $scope.choiceFactor);
+			}
 		})
 }
 
@@ -92,9 +97,59 @@ function modelChanged($scope,$http)
 	}
 }
 
-function factorChanged($scope,$http,url)
+function backmodelChanged($scope,$http,modelid,factorid)
+{
+		//alert(id);
+		for(var i=0;i<$scope.modelinfos.length;i++)
+		{
+			if($scope.modelinfos[i]._id==modelid)
+			{
+
+				$scope.modelinfodetail=$scope.modelinfos[i];
+				//alert($scope.modelinfodetail.min);
+			}
+		}
+		//alert(url_ratinglistbymodelidangular_scala+"/"+id);
+		if($scope.modelinfodetail.status=='draft')
+		{
+			$('#btnInsert').show();
+		}
+		else
+		{
+			$('#btnInsert').hide();
+		}
+
+		$http.post(url_factoroptionbymodelid_scala, {modelid: modelid}).
+			success(function (data, status, headers, config) {
+				//console.log(data["SUCCESS"]);
+				//alert(data);
+				$scope.factors=[];
+				if(typeof data["ERROR"]=='undefined')
+				{
+					$scope.factors = data["SUCCESS"];
+					for(var i=0;i<$scope.factors.length;i++)
+					{
+						if($scope.factors[i]._id==factorid)
+						{
+							//alert($scope.factors[i]["FactorOption"][0].FactorOptionName);
+
+							$scope.factoroptions = $scope.factors[i]["FactorOption"];
+
+						}
+					}
+				}
+			}).
+			error(function (data, status, headers, config) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+			});
+
+}
+
+function factorChanged($scope,$http)
 {
 	$scope.factorChanged = function(id){
+		//alert(id)
 		for(var i=0;i<$scope.factors.length;i++)
 		{
 			if($scope.factors[i]._id==id)
@@ -110,7 +165,7 @@ function factorChanged($scope,$http,url)
 
 function getfactoroptiondetailangular($scope,$http,url,factorId,factorOptionId)
 {
-	//alert(url)
+	//alert(factorId)
 	$http.post(url, {factorId:factorId,factorOptionId:factorOptionId}).
 		success(function(data, status, headers, config) {
 			$scope.factoroptiondetail = data["FactorsOptionItem"];
@@ -124,7 +179,7 @@ function getfactoroptiondetailangular($scope,$http,url,factorId,factorOptionId)
 }
 function factoroptionadd($scope,$http) {
 	$scope.factoroptionadd = function() {
-		window.location.assign("/factoroptiondetail.html?modelid=" + $scope.choiceModel);
+		window.location.assign("/factoroptiondetail.html?modelid=" + $scope.choiceModel+"&factorId="+$scope.choiceFactor);
 	}
 }
 
@@ -193,7 +248,7 @@ function actionfactoroptiondetailangular($scope,$http)
 		}
 		$http.post(url,  angular.toJson(factoroptions)).
 		  success(function(data, status, headers, config) {
-			window.location.assign("/factoroptions.html")
+			window.location.assign("/factoroptions.html?modelid="+$scope.choiceModel+"&factorid="+$scope.choiceFactor)
 		  }).
 		  error(function(data, status, headers, config) {
 			// called asynchronously if an error occurs
